29b60f04e7749d8974c50d5a1a28abb7b20c4ea1
Alvin Chua <alvin.j.chua@jpl.nasa.gov>
2019-10-15 22:21:04 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,86 @@
-# sampling-manifold-restricted-gaussians
+# Sampling from manifold-restricted Gaussians
+
+Python implementation of algorithm from [[A. J. K. Chua, Sampling from manifold-restricted distributions using tangent bundle projections, Stat. Comput., in press]](https://doi.org/10.1007/s11222-019-09907-8). Contains iteration class with documentation and example usage.
+
+**<https://arxiv.org/abs/1811.05494>**
+
+---
+
+## algorithm.iteration
+
+### METHODS
+
+**\__init\__**(*betastar, minisize=100, epsilon=1e-2, bounds=None, autocompact=True, replace=True*)
+
+**\__call\__**(*theta, alpha, jacobian, hessian=None*)
+
+### ARGUMENTS
+
+**betastar**: *float array with shape (d,)*  
+\- beta_* from Eq. (25) in manuscript
+
+**minisize**: *int, optional*  
+\- size of mini-distribution (denoted m in manuscript)
+
+**epsilon**: *float, optional*  
+\- tuning parameter for overall spread of mini-distribution
+
+**bounds**: *float array with shape (s,2), optional*  
+\- sampling limits, i.e., [[min(theta^1),max(theta^1)],...,[min(theta^s),max(theta^s)]]  
+\- if not provided, constant compactness is used
+
+**autocompact**: *bool, optional*  
+\- set false to force use of constant compactness
+
+**replace**: *bool, optional*  
+\- set false to retain generated points outside sampling limits
+
+**theta**: *float array with shape (s,)*  
+\- base point in sample space (denoted theta_i in manuscript)
+
+**alpha**: *float array with shape (d,)*  
+\- map evaluated at base point (denoted alpha_i in manuscript)
+
+**jacobian**: *float array with shape (d,s)*  
+\- first derivative of map evaluated at base point (denoted J_i in manuscript)
+
+**hessian**: *float array with shape (d,s,s), optional*  
+\- second derivative of map evaluated at base point (denoted H_i in manuscript)  
+\- if not provided, constant or metric-only compactness is used
+
+### ATTRIBUTES
+
+**samples**: *float array with shape (m,s)*  
+\- generated points in sample space  
+\- theta_ij from Eq. (13) in manuscript
+
+**logweights**: *float array with shape (m,)*  
+\- natural logarithm of sample weights  
+\- w_ij from Eqs (30), (41), (42) in manuscript
+
+**metric**: *float array with shape (s,s)*  
+\- (F_I)_i from Eq. (3) in manuscript
+
+**pseudoinverse**: *float array with shape (s,d)*  
+\- (J^+)_i from Eq. (9) in manuscript
+
+**metricterm**: *float*  
+\- (lambda^2)_i from Eqs (37), (38) in manuscript
+
+**second**: *float array with shape (s,s)*  
+\- (F_II)_i from Eqs (6), (7) in manuscript
+
+**curvatureterm**: *float*  
+\- kappa_i from Eqs (33)-(35) in manuscript
+
+**compactness**: *float*  
+\- c_i from Eq. (40) in manuscript
+
+**beta**: *float array with shape (m,d)*  
+\- beta_ij from Eq. (5) in manuscript
+
+**pushforward**: *float array with shape (m,d)*  
+\- (beta^perp)_ij from Eq. (14) in manuscript
+
+**timing**: *float array with shape (6,)*  
+\- timing of Steps 2-6 and boundary corrections in manuscript
---
+++ algorithm.py
@@ -0,0 +1,65 @@
+# definition of iteration class
+
+import numpy as np
+import time
+
+class iteration:
+    
+    def __init__(self, betastar, minisize=100, epsilon=1e-2, bounds=None, autocompact=True, replace=True):
+        
+        self.betastar = betastar
+        self.minisize = minisize
+        self.epsilon = epsilon
+        self.bounds = bounds
+        self.autocompact = autocompact
+        self.replace = replace
+        
+    def __call__(self, theta, alpha, jacobian, hessian=None):
+        
+        sdim = theta.size
+        ddim = alpha.size
+        self.timing = [time.time()]
+        
+        self.metric = np.einsum('ij,ik->jk', jacobian, jacobian)
+        invmet = np.linalg.inv(self.metric)
+        self.pseudoinverse = np.einsum('ij,kj->ik', invmet, jacobian)
+        self.timing.append(time.time())
+        
+        self.compactness = 1/self.epsilon
+        if self.autocompact and (self.bounds is not None):
+            sqrtlam = np.einsum('ij,jk->ik', np.diag(2/np.diff(self.bounds)[:,0]), self.pseudoinverse)
+            self.metricterm = np.amax(np.linalg.svd(sqrtlam)[1]**2)
+            self.compactness = self.metricterm/self.epsilon
+        else: self.metricterm = None
+        if (self.metricterm is not None) and (hessian is not None):
+            evals, evecs = np.linalg.eig(invmet)
+            qmat = np.einsum('ij,jk->ik', evecs, np.diag(np.sqrt(evals)))
+            ii = hessian-np.einsum('ij,jkl->ikl', jacobian, np.einsum('ij,jkl->ikl', self.pseudoinverse, hessian))
+            self.second = np.linalg.norm(ii, axis=0)
+            kmat = np.einsum('ji,jk->ik', qmat, np.einsum('ij,jk->ik', self.second, qmat))
+            self.curvatureterm = np.amax(np.linalg.eig(kmat)[0])
+            self.compactness = max(self.metricterm, self.curvatureterm)/self.epsilon
+        else: self.curvatureterm = None
+        self.timing.append(time.time())
+        
+        self.beta = np.random.multivariate_normal(alpha, np.eye(ddim)/self.compactness, self.minisize)
+        self.timing.append(time.time())
+        
+        self.samples = theta+np.einsum('ij,kj->ki', self.pseudoinverse, self.beta-alpha)
+        self.timing.append(time.time())
+        
+        self.pushforward = alpha+np.einsum('ij,kj->ki', jacobian, self.samples-theta)
+        logfac = np.log((1+self.compactness)/self.compactness)*sdim/2
+        lvec = np.einsum('ji,kj->ki', jacobian, self.pushforward-self.betastar)
+        rvec = np.einsum('ij,kj->ki', self.pseudoinverse, self.pushforward-self.betastar)
+        negexp = np.sum(lvec*rvec, axis=1)/(1+self.compactness)/2
+        self.logweights = logfac-negexp
+        self.timing.append(time.time())
+        
+        if self.replace and (self.bounds is not None):
+            mask = np.any((self.samples<self.bounds[:,0])|(self.samples>self.bounds[:,1]), axis=1)
+            self.samples[mask] = np.tile(theta, [np.sum(mask),1])
+            self.logweights[mask] = np.tile(0, np.sum(mask))
+        self.timing.append(time.time())
+        
+        self.timing = np.diff(np.array(self.timing))
